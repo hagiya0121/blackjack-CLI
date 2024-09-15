083d3c0a08c022959067ffd88d1252f43f3e162c
hagiya0121 <koazu0121@gmail.com>
2024-09-15 16:08:54 +0900

    feat: カードクラスとデッキクラスを実装

--- bin/blackjack.js
+++ bin/blackjack.js
@@ -1,1 +1,39 @@
 #!/usr/bin/env node
+const SUITS = ["heart", "diamond", "club", "spade"];
+const RANKS = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
+
+class Card {
+  constructor(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+  }
+}
+
+class Deck {
+  constructor() {
+    this.cards = this.#createCards();
+  }
+
+  #createCards() {
+    return SUITS.flatMap((suit) => {
+      return RANKS.map((rank) => new Card(suit, rank));
+    });
+  }
+}
+
+const deck = new Deck();
+console.log(deck.cards);
